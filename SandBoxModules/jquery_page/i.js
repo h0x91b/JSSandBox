08bc1d61030549aa9294f13d412ea9823bacc876
h0x91b <h0x91b@gmail.com>
2013-03-07 15:49:56 +0200

    [+] load two jqueries with different version in same time

--- SandBoxModules/jquery_page/i.js
+++ SandBoxModules/jquery_page/i.js
@@ -10,7 +10,7 @@ console.log('jquery_page router loaded')
 _ready = false //i will need some modules, so wait...
 exports = {} //nothing to export
 
-global.require(['main/timeout','main/jQuery-1.9.1'],function(timeout,$){
+global.require(['main/timeout','main/jQuery-1.9.1','main/jQuery-1.6.3'],function(timeout,$,$163){
 	
 	console.log('jquery there =)',$)
 	
@@ -21,6 +21,11 @@ global.require(['main/timeout','main/jQuery-1.9.1'],function(timeout,$){
 		$p.load('SandBox.html h1',function(){
 			$p.after($('<p>html loaded, jQuery works</p>'))
 		});
+		$("body").append('<h3>I am h3 added by jQuery 1.9.1</h3>');
+	})
+	$163(document).ready(function(){
+		console.log('jQuery 1.6.3 also there');
+		$("body").append('<h3>I am h3 added by jQuery 1.6.3</h3>');
 	})
 	//all modules loaded, so i am a ready
 	_ready = true;
